bd8f99b66a792544e95e5c64f59eaf342101b979
Kareem Hussein <futurecitytaxi@gmail.com>
2024-07-20 15:59:33 +0300

    init backend

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = 8000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello from the backend!');
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
